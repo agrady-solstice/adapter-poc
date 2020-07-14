34784e2b76a7fd11ddbbdbd50872b6570050be87
Aaron Grady <agrady@solstice.com>
2020-07-14 08:52:43 -0500

    update

--- src/main/java/com/mq/adapterpoc/processor/MessageProcessor.java
+++ src/main/java/com/mq/adapterpoc/processor/MessageProcessor.java
@@ -10,27 +10,5 @@ import javax.jms.TextMessage;
 
 @Slf4j
 public class MessageProcessor {
-//
-//    public void processMessage(Message msg){
-//        String txtMsg = null;
-//        String txIdr = null;
-//        try {
-//            log.info(">>processMessage started>>");
-//            if (msg instanceof BytesMessage)
-//                txtMsg = getByteMessage(msg);
-//            else
-//                txtMsg = ((TextMessage) msg).getText();
-//            txIdr = MessageUtil.getSwiftMessageId(txtMsg);
-//            log.info("Message ID in LISTENER::" + txIdr);
-//            swiftProcess.processMessages(txtMsg, txIdr);
-//        } catch (JmsException | JMSException e) {
-//            emailProcessor.sendEmail("Failed to process "+txtMsg.toString(), e.getMessage());
-//            log.error("Got JMSException error in gsl3plsr-adapter-flow" + e.getMessage());
-//            auditBuilder.auditFailed(txtMsg, e.getMessage(), "GSL3PLSwiftReceive");
-//
-//        }
-//
-//
-//
-//    }
+
 }
